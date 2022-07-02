64242912094611c900c674f5dcbcfe67b46c726e
mohbayati <m.bayati14@gmail.com>
2022-07-02 20:55:37 +0430

    Adding the shopping cart feature-2

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { LoadingButton } from "@mui/lab";
 import {
   Divider,
   Grid,
@@ -5,25 +6,57 @@ import {
   TableBody,
   TableCell,
   TableRow,
+  TextField,
   Typography,
 } from "@mui/material";
+import { removeListener } from "process";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import agent from "../../app/api/agent";
 import NotFound from "../../app/errors/NotFound";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
+import { useStoreContext } from "../../context/StoreContext";
 
 export default function ProductDetails() {
+  const { basket, setBasket, removeItem } = useStoreContext();
   const { id } = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState(true);
+  const [submiting, setSubmiting] = useState(false);
+  const [quantity, setQuantity] = useState(0);
+  const item = basket?.items.find((i) => i.productId === product?.id);
+
+  function handlUpdateCart() {
+    setSubmiting(true);
+    if (!item || quantity > item.quantity) {
+      const updatedQuantity = item ? quantity - item.quantity : quantity;
+      agent.Basket.addItem(product?.id!, updatedQuantity)
+        .then((basket) => setBasket(basket))
+        .catch((e) => console.log(e))
+        .finally(() => setSubmiting(false));
+    } else {
+      const updatedQuantity = item.quantity - quantity;
+      agent.Basket.removeItem(product?.id!, updatedQuantity)
+        .then((basket) => removeItem(product?.id!, updatedQuantity))
+        .catch((e) => console.log(e))
+        .finally(() => setSubmiting(false));
+    }
+  }
+  function handlInputChange(event: any) {
+    if (event.target.value >= 0) {
+      setQuantity(parseInt(event.target.value));
+      console.log(quantity);
+    }
+  }
+
   useEffect(() => {
+    if (item) setQuantity(item.quantity);
     agent.Catalog.details(parseInt(id!))
       .then((response) => setProduct(response))
       .catch((error) => console.log(error))
       .finally(() => setLoading(false));
-  }, [id]);
+  }, [id, item]);
   if (loading) return <LoadingComponent message="Loading Product..." />;
   if (!product) return <NotFound />;
   return (
@@ -65,6 +98,34 @@ export default function ProductDetails() {
             </TableRow>
           </TableBody>
         </Table>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              type="number"
+              variant="outlined"
+              label="Quantity in Cart"
+              fullWidth
+              value={quantity}
+              onChange={handlInputChange}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <LoadingButton
+              disabled={
+                item?.quantity! === quantity || (!item && quantity === 0)
+              }
+              loading={submiting}
+              onClick={handlUpdateCart}
+              color="primary"
+              size="large"
+              variant="contained"
+              fullWidth
+              sx={{ height: "55px" }}
+            >
+              {item ? "Update Quantity" : "Add to Cart"}
+            </LoadingButton>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
